7524a83c201d26d838819732084b3ce88e696faa
Niklas Merz <niklasmerz@linux.com>
2020-07-26 20:51:59 +0200

    Add getCategory function

---
+++ functions/getCategory/source.js
@@ -0,0 +1,5 @@
+exports = function(id){
+  var collection = context.services.get("mongodb-atlas").db("finanztopf").collection("categories");
+  var doc = collection.findOne({id: id});
+  return doc;
+};
