65cca20806c45e7823c4b67b51d13268a1b1beef
Niklas Merz <niklasmerz@linux.com>
2020-07-26 20:23:31 +0200

    Initial commit

---
+++ functions/addCategory/source.js
@@ -0,0 +1,14 @@
+exports = function(category){
+  
+    //Accessing application's values:
+    //var x = context.values.get("value_name");
+
+    //Accessing a mongodb service:
+    var collection = context.services.get("mongodb-atlas").db("finanztopf").collection("categories");
+    var doc = collection.insertOne(category);
+
+    //To call other named functions:
+    //var result = context.functions.execute("function_name", arg1, arg2);
+
+  return {arg: doc};
+};
