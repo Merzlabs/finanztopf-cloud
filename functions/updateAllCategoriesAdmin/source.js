9f383ab31cc26429312ddbfa1e2578ebc5085a41
Niklas Merz <niklasmerz@linux.com>
2020-07-28 12:02:11 +0200

    Use updateMany

--- functions/updateAllCategoriesAdmin/source.js
+++ functions/updateAllCategoriesAdmin/source.js
@@ -4,7 +4,7 @@ exports = function(docs){
   try {
     if (activeUser.data.email && activeUser.data.email.indexOf("@merzlabs.com") > 0) {
       var collection = context.services.get("mongodb-atlas").db("finanztopf").collection("categories");
-      var res = collection.update({}, docs);
+      var res = collection.updateMany({}, docs);
       return res;
     } else {
       throw Error("This user is not allowed to execute the system function")
