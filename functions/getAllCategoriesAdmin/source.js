2c4952ed658ac2f58643e6fc6c814dcafb3f9083
Niklas Merz <niklasmerz@linux.com>
2020-07-28 11:34:32 +0200

    Try securing admin functions

--- functions/getAllCategoriesAdmin/source.js
+++ functions/getAllCategoriesAdmin/source.js
@@ -1,5 +1,14 @@
 exports = function(){
-  var collection = context.services.get("mongodb-atlas").db("finanztopf").collection("categories");
-  var docs = collection.find({});
-  return docs;
+  const activeUser = context.user;
+  try {
+    if (activeUser.identities[0]?.data?.email?.indexOf("@merzlabs.com")) {
+      var collection = context.services.get("mongodb-atlas").db("finanztopf").collection("categories");
+      var docs = collection.find({});
+      return docs;
+    } else {
+      return null;
+    }
+  } catch(e) {
+    return null;
+  }
 };
