f34f86a7386e8b6a69a34a7d60ca16cce0c474e1
Niklas Merz <niklasmerz@linux.com>
2020-07-28 11:56:20 +0200

    Add admin update function

---
+++ functions/updateAllCategoriesAdmin/source.js
@@ -0,0 +1,15 @@
+exports = function(docs){
+  const activeUser = context.user;
+
+  try {
+    if (activeUser.data.email && activeUser.data.email.indexOf("@merzlabs.com") > 0) {
+      var collection = context.services.get("mongodb-atlas").db("finanztopf").collection("categories");
+      var res = collection.update({}, docs);
+      return res;
+    } else {
+      throw Error("This user is not allowed to execute the system function")
+    }
+  } catch(e) {
+    throw e;
+  }
+};
