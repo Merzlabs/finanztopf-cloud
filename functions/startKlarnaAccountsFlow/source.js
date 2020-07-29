a6b40208d900a6f702cfef965002d45bcd23118d
Niklas Merz <niklasmerz@linux.com>
2020-07-29 22:16:29 +0200

    Accounts flow function

---
+++ functions/startKlarnaAccountsFlow/source.js
@@ -0,0 +1,11 @@
+exports = async function(url){
+  const headers = {"Authorization": [`Bearer ${context.values.get("KLARNA_TOKEN")}`]}
+
+  const response = await context.http.put({
+    url: url,
+    encodeBodyAsJSON: true,
+    headers: headers
+  })
+  // The response body is a BSON.Binary object. Parse it and return.
+  return EJSON.parse(response.body.text());
+};
